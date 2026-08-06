4984f8806ea794f98807e9fa43994cd2fa4b7014
Edward-Mercado <edwardmercado149@gmail.com>
2026-08-05 22:48:24 -0400

    adsf

--- app/app/components/cutscenes/additional-content.vue
+++ app/app/components/cutscenes/additional-content.vue
@@ -1,15 +1,29 @@
 <template>
-    <img :src="prop.currentLine.backgroundURL" class="absolute bg-white h-[80%] w-[90%] top-[10%] border-4 border-slate-800 shadow-xl">
+    <transition name="fade">
+        <img :src="prop.currentLine.backgroundURL" class="absolute bg-white h-[80%] w-[90%] top-[10%] border-4 border-slate-800 shadow-xl">
         
-    </img>
+        </img>
+    </transition>
 </template>
 
 <script setup lang="ts">
 const prop = defineProps<{
     currentLine: DialogueLine
 }>()
+const fadeSpeed = ref<number>(prop.currentLine.speed * 2)
 </script>
 
 <style scoped>
-
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0
+}
+.fade-enter-to,
+.fade-leave-from {
+    opacity: 1
+}
+.fade-enter-active,
+.fade-leave-active {
+    transition: all ease-in-out v-bind(fadeSpeed)
+}
 </style>
